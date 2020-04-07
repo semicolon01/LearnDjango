d607d9c2bc2f0191e5d444d51f7d5930448ef436
semicolon01 <movaffaghmahsa@gmail.com>
2020-04-07 15:51:22 -0700

    add template and use it in views.py

--- myProject/blog/views.py
+++ myProject/blog/views.py
@@ -5,25 +5,23 @@ from django.http import HttpResponse, JsonResponse
 # home page
 
 def home(request):
-    return HttpResponse("Hello Django!")
-
-def api(request):
-    data = {
-        "1": {
-            "title" : "hi1",
-            "id" : 20,
-            "slug" : "first-article"
-        },
-        "2": {
-            "title": "hi2",
-            "id": 21,
-            "slug": "second-article"
-        },
-        "3": {
-            "title": "hi3",
-            "id": 22,
-            "slug": "third-article"
-        }
+    context = {
+        "articles":[
+            {
+                "title": "One Punch Man",
+                "description": "The seemingly ordinary and unimpressive Saitama has a rather unique hobby: being a hero. In order to pursue his childhood dream, he trained relentlessly for three years—and lost all of his hair in the process.",
+                "img": "https://animelist.pw/public/storage/anime/image/2019/5/1556893725-c9261bbb.jpg"
+            },
+            {
+                "title": "Shingeki no Kyojin",
+                "description": "Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called titans, forcing humans to hide in fear behind enormous concentric walls.",
+                "img": "https://animelist.pw/public/storage/anime/image/2019/5/1556818737-6a418682.jpg"
+            },
+            {
+                "title": "Yakusoku no Neverland",
+                "description": "Surrounded by a forest and a gated entrance, the Grace Field House is inhabited by orphans happily living together as one big family, looked after by their Mama, Isabella. Although they are required to take tests daily, the children are free to spend their time as they see fit, usually playing outside, as long as they do not venture too far from the orphanage—a rule they are expected to follow no matter what.",
+                "img": "https://animelist.pw/public/storage/anime/image/2019/5/1556669835-4f1ad7a2.jpg"
+            }
+        ]
     }
-
-    return JsonResponse(data)
+    return render(request, "blog/home.html", context)
