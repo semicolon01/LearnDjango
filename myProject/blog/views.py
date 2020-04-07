7b8df00aaa8652ae2d53b87a92edb8991c57d9e7
semicolon01 <movaffaghmahsa@gmail.com>
2020-04-07 14:44:29 -0700

    simple jasonResponse and separate each app's urls.py file

--- myProject/blog/views.py
+++ myProject/blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 # home page
 
 def home(request):
     return HttpResponse("Hello Django!")
+
+def api(request):
+    data = {
+        "1": {
+            "title" : "hi1",
+            "id" : 20,
+            "slug" : "first-article"
+        },
+        "2": {
+            "title": "hi2",
+            "id": 21,
+            "slug": "second-article"
+        },
+        "3": {
+            "title": "hi3",
+            "id": 22,
+            "slug": "third-article"
+        }
+    }
+
+    return JsonResponse(data)
